ca1a4bb8cf5d050c469260bc1aabba86cddf9efc
Sowmya D <88073238+Sowmyad15@users.noreply.github.com>
2023-06-16 15:19:40 +0530

    Time: 508 ms (22.51%), Space: 28.2 MB (73.21%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d={}
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[nums[i]]=1
+            else:
+                d[nums[i]]+=1
+                if d[nums[i]]>=2:
+                    return True
+        return False
