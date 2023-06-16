776415d24cdcaac55951a35d2b04131ca76d7a7c
Sowmya D <88073238+Sowmyad15@users.noreply.github.com>
2023-06-16 15:41:38 +0530

    Time: 3735 ms (5%), Space: 13.8 MB (82.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+           return False
+        else:
+            for ch in s:
+                if s.count(ch)!=t.count(ch):
+                    return False
+        return True
